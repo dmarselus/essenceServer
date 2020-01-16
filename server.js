87d160b3f69701af7ca241691c4297a9b96c5063
david marselus <marcellloki@gmail.com>
2020-01-15 22:11:05 -0600

    working1

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const http = require('http');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const hostname = '127.0.0.1';
 const port = 3000;
+const url = 'mongodb+srv://admin:admin@cluster0-8d3rr.mongodb.net/test?retryWrites=true&w=majority';
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
+// Database Name
+const dbName = 'essence';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+	assert.equal(null, err);
+	console.log('Connected successfully to server');
+	let arr = [];
+	const db = client.db(dbName);
+	const test = db.collection('users').find();
+	test.forEach(function(item, err) {
+		console.log(item);
+	});
+	// console.log(arr);
+
+	// db.collectionNames('users', function(err, items) {
+	// 	console.log(items);
+	// });
+
+	client.close();
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = http.createServer((req, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader('Content-Type', 'text/plain');
+// 	res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+// 	console.log(`Server running at http://${hostname}:${port}/`);
+// });
